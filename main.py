1594d7d981cd2b018dbc8613b52859bad645c952
niccolot <nico_tiezzi98@hotmail.it>
2023-06-01 11:41:50 +0200

    train loop with validation

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from torch_geometric.datasets import MD17
 from torch_geometric.loader import DataLoader
 import gnnff
 import torch
-from torcheval.metrics import MeanSquaredError as MSE
 from matplotlib import pyplot as plt
 import time
-
+import train_utils
 
 batch_size = 1  
 node_channels = 4
@@ -19,7 +18,7 @@ dataset_train = MD17(root='aspirin_dataset', name='aspirin CCSD', train=True)
 dataset_val = MD17(root='aspirin_dataset', name='aspirin CCSD', train=False)
 
 train_loader = DataLoader(dataset_train, batch_size=batch_size)
-
+val_loader = DataLoader(dataset_val, batch_size=batch_size)
 
 model = gnnff.GNNFF(hidden_node_channels=node_channels,
                     hidden_edge_channels=edge_channels,
@@ -29,21 +28,24 @@ optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 criterion = torch.nn.MSELoss()
 
 losses = []
+val_losses = []
 
-model.train()
 for epoch in range(epochs):
     start = time.time()
-    for iter, data in enumerate(train_loader):
-        optimizer.zero_grad()
-        out = model(data.z, data.pos)
-        loss = criterion(out, data.force)
-        loss.backward()
-        optimizer.step()
-        losses.append(loss.item())
+    model.train()
+    mean_train_loss = train_utils.train_loop(model, train_loader, criterion, optimizer, losses)
+    model.eval()
+    mean_val_loss  = train_utils.val_loop(model, val_loader, criterion, val_losses)
     end = time.time()
     elapsed = end-start
-    print("Epoch: %d done, time: %.1f" %(epoch, elapsed))
+    print("\nEpoch %d done, time: %.1f s" % (epoch+1, elapsed))
+    print("mean_train_loss: %.2f, mean_val_loss: %.2f" % (mean_train_loss, mean_val_loss))
 
-plt.plot(losses, label='MSE')
+
+plt.plot(losses, label='Train MSE')
+plt.plot(val_losses, label='Validation MSE')
 plt.xlabel('steps')
+plt.ylabel('MSE')
+plt.legend()
 plt.show()
+
---
+++ train_utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def train_loop(model, train_loader, loss_fn, optimizer, losses_list):
+
+    for iter, data in enumerate(train_loader):
+
+        optimizer.zero_grad()
+        out = model(data.z, data.pos)
+        loss = loss_fn(out, data.force)
+        loss.backward()
+        optimizer.step()
+        losses_list.append(loss.item()/len(data))
+
+    return np.mean(losses_list)
+
+
+def val_loop(model, val_loader, loss_fn, val_losses_list):
+
+    for iter, data in enumerate(val_loader):
+
+        out = model(data.z, data.pos)
+        loss = loss_fn(out, data.force)
+        val_losses_list.append(loss.item()/len(data))
+
+    return np.mean(val_losses_list)
+
