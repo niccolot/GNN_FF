97e4319d186e57d551648f8070ec77af26b8e811
Lorenzo Castelli <83464607+LoreCstll@users.noreply.github.com>
2023-07-12 11:04:05 +0200

    Update PBCmain.py

--- Periodic Extension/PBCmain.py
+++ Periodic Extension/PBCmain.py
@@ -7,31 +7,24 @@ import time
 import train_utils
 
 batch_size = 1 
-node_channels = 1
-edge_channels = 50
-layers = 4
-epochs = 5
-lr = 2e-3
+node_channels = 2
+edge_channels = 100
+layers = 3
+epochs = 4
+lr = 1e-3
 
-DATA_PATH= '/home/lorenz_gauge/Documenti/Lorenzo/Esami_Magistrale/Computing/Progetto_Esame/data/processed/tensor_dataset2.pt'
+DATA_PATH= '/home/castelli/Documents/GNNFF/Progetto_Esame/data/processed/tensor_dataset1.pt'
+# load data
 dataset_train = torch.load(DATA_PATH)
 
 
+# make train and validation dataset
 data1, data2 = torch.utils.data.random_split(dataset_train, [0.5,0.5])
 print(len(data1),len(data2))
+# orginize and shuffle the dataset into an iterable
 train_loader = DataLoader(data1, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(data2, batch_size=batch_size, shuffle=True)
 
-
-#print(dataset_train[0].bounds)
-#train_loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
-
-
-#i = 0
-#for data in train_loader:
-#    print(data.size(),i,data[0].bounds)
-#    i+=1
-    #break
 
 model = GNNFF_uff.GNNFF(hidden_node_channels=node_channels,
                     hidden_edge_channels=edge_channels,
@@ -42,13 +35,17 @@ criterion = torch.nn.MSELoss()
 
 losses = []
 val_losses = []
-#"""
+
+# loop over the epochs
 for epoch in range(epochs):
+    
     start = time.time()
+    
     model.train()
     mean_train_loss = train_utils.train_loop(model, train_loader, criterion, optimizer, losses)
     model.eval()
     mean_val_loss  = train_utils.val_loop(model, val_loader, criterion, val_losses)
+    
     end = time.time()
     elapsed = end-start
     print("\nEpoch %d done, time: %.1f s" % (epoch+1, elapsed))
@@ -61,5 +58,3 @@ plt.xlabel('steps')
 plt.ylabel('MSE')
 plt.legend()
 plt.show()
-
-#"""
